cbd6f0e5b774b0c19f8ceb068f92a46b5d5fb5d8
Hardelele <Hardelele@yahoo.com>
2020-10-30 14:25:48 +0300

    Complete realization of CounterPlugin

--- counter-plugin/src/main/java/ru/digitalhabbits/homework1/plugin/CounterPlugin.java
+++ counter-plugin/src/main/java/ru/digitalhabbits/homework1/plugin/CounterPlugin.java
@@ -2,6 +2,8 @@ package ru.digitalhabbits.homework1.plugin;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CounterPlugin
         implements PluginInterface {
@@ -9,7 +11,18 @@ public class CounterPlugin
     @Nullable
     @Override
     public String apply(@Nonnull String text) {
-        // TODO: NotImplemented
-        return "CounterPlugin: NotImplemented";
+        int linesCount = text.split("\n").length;
+        int wordsCount = countWords(text);
+        int lettersCount = text.toCharArray().length;
+        return linesCount + ";" + wordsCount + ";" + lettersCount;
+    }
+
+    private int countWords(String text) {
+        Matcher matcher = Pattern.compile("(\\b[a-zA-Z][a-zA-Z.0-9-]*\\b)").matcher(text);
+        int counter = 0;
+        while(matcher.find()){
+            counter++;
+        }
+        return counter;
     }
 }
